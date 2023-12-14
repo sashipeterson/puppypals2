4d065c704b27e6fe1b2be542e113232bd4821fbb
Hisashi Peterson <146689382+sashipeterson@users.noreply.github.com>
2023-12-14 13:49:54 +0900

    Made further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
+import { puppyList } from './data.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+ //console.log("puppyList: ", puppyList);
+ const featuredPup = puppies.find((pup) => pup.id === featPupId);
+ console.log(featuredPup);
+
   return (
-      <div className="card">
-       
+   <div className="App">
+
+    {featPupId && (
+      <div class="featuredPups">
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
       </div>
-  )
+    )}
+
+    {puppies.map((puppy) => {
+      return (
+        <p onClick={() => { setFeatPupId(puppy.id)}}>
+          {puppy.name}
+        </p>
+      );
+    })}
+   </div>
+  );
 }
 
 export default App
